ed94251176dd5f8199e6936b355ece15fb3ca4e2
GuillermoCzza <guiczza@gmail.com>
2024-01-25 11:46:05 -0300

    create RSS .json database files if not exist

--- events/ready.js
+++ events/ready.js
@@ -1,13 +1,33 @@
 const { Events } = require('discord.js');
 const CronJob = require('cron').CronJob;
 const config = require(__dirname + "/../config.json");
-const RSSFolderPath = "../RSS_news_discord_messenger/";
-const updatesChannelsIDs = require(RSSFolderPath + "updates-channels.json");
+const path = require('path');
+const fs = require('fs');
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
+		const RSSFolderPath = path.join(process.cwd(), "/RSS_news_discord_messenger/");
+
+		const updatesChannelsIDsPath = RSSFolderPath + "updates-channels.json";
+		const sentItemsGUIDsPath = RSSFolderPath + "already-sent-items-guids.json";
+		const termRegexWhitelistPath = RSSFolderPath + "term-regex-whitelist.json";
+
+		//if not yet existing (for example, if repo has just been downloaded), create the database .json files with empty arrays
+		if (!fs.existsSync(updatesChannelsIDsPath)) {
+			fs.writeFileSync(updatesChannelsIDsPath, '[]');
+		}
+		if (!fs.existsSync(sentItemsGUIDsPath)) {
+			fs.writeFileSync(sentItemsGUIDsPath, '[]');
+		}
+		if (!fs.existsSync(termRegexWhitelistPath)) {
+			fs.writeFileSync(termRegexWhitelistPath, '[]');
+		}
+
+
+		const updatesChannelsIDs = require(updatesChannelsIDsPath);
+
 		//set up RSS reader
 		const updatesChannels = updatesChannelsIDs.map(channelID => client.channels.cache.get(channelID));
 		const RSSNewsDiscordMessenger = require(RSSFolderPath + "RSSNewsDiscordMessenger.js");
@@ -18,7 +38,6 @@ module.exports = {
 			onTick: async () => RSSNewsDiscordMessenger.lookForRSSNews(updatesChannels)
 		});
 		RSSNewsJob.start();
-
 
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
